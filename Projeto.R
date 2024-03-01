76079d1d0b561ca6c086498fc2c0855780e71e31
avena <fernando.avena@gmail.com>
2024-03-01 15:41:13 -0300

    Ate Fim do Projeto

--- Projeto.R
+++ Projeto.R
@@ -26,8 +26,11 @@ arrange(shp_br, desc(VarPerc))
 arrange(shp_br, desc(Pop2022))
 
 ## Minas Gerais
-shp_mg = filter(shp_br, nam_stt == 'Minas Gerais')
-head(shp_mg)
+# shp_mg = filter(shp_br, nam_stt == 'Minas Gerais')
+# head(shp_mg)
+
+shp_rs = filter(shp_br, nam_stt == 'Rio Grande do Sul')
+head(shp_rs)
 
 
 ggplot(data = shp_mg, aes(x = Pop2022, y = Pop2010)) + 
@@ -36,6 +39,49 @@ ggplot(data = shp_mg, aes(x = Pop2022, y = Pop2010)) +
 
 
 ### Plotando os Mapas
+# ggplot() + 
+#   geom_sf(data = shp_mg, col = 'black', linewidth = 0.1, aes(fill = VarPerc)) + 
+#   scale_fill_gradientn( colors = brewer.pal(name = "Spectral", n = 11)  ) + 
+#   theme_bw() +
+#   labs(title = 'Taxa de Crescimento Populacional em (%) - Minas Gerais', 
+#        subtitle = 'Fonte: Censo 2022 - IBGE') +
+#   annotation_north_arrow(location = 'tr') + 
+#   annotation_scale() + 
+#   theme(legend.position = 'bottom',
+#         axis.text = element_text(size = 30),
+#         plot.title = element_text(size = 25, hjust = 0.5),
+#         plot.subtitle = element_text(size = 10, hjust = 0.5),
+#         legend.key.size = unit(0.5, 'cm')
+#         )
+
+## plot para RS
 ggplot() + 
-  geom_sf(data = shp_mg, col = 'black', linewidth = 0.1, aes(fill = VarPerc)) + 
-  scale_fill_gradientn( colors = brewer.pal(name = "Spectral", n = 11)  )
+  geom_sf(data = shp_rs, col = 'black', linewidth = 0.1, aes(fill = VarPerc)) + 
+  scale_fill_gradientn( colors = brewer.pal(name = "Spectral", n = 11)  ) + 
+  theme_bw() +
+  labs(title = 'Taxa de Crescimento Populacional em (%) - Rio Grande do Sul', 
+       subtitle = 'Fonte: Censo 2022 - IBGE') +
+  annotation_north_arrow(location = 'tr') + 
+  annotation_scale() + 
+  theme(legend.position = 'bottom',
+        axis.text = element_text(size = 30),
+        plot.title = element_text(size = 25, hjust = 0.5),
+        plot.subtitle = element_text(size = 10, hjust = 0.5),
+        legend.key.size = unit(0.5, 'cm')
+  )
+
+
+## Exportar
+# ggsave(filename = 'Resultados/tx_crescimento_mg_2022.jpeg',
+#        width = 20,
+#        height = 20,
+#        units = 'in',
+#        dpi = 300)
+
+
+
+ggsave(filename = 'Resultados/tx_crescimento_rs_2022.jpeg',
+       width = 20,
+       height = 20,
+       units = 'in',
+       dpi = 300)
